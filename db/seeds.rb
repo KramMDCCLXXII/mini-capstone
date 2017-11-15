2fccb944c4a9907aa0c651bee7d3d3f08ef4963e
Mark Custard <markc1772@gmail.com>
2017-11-14 19:01:43 -0600

    added products to my store

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Product.create({name: "Volcano", price: "$500", image: "https://1ikehw1twddx1nfo883n2mn5-wpengine.netdna-ssl.com/wp-content/uploads/2016/12/IMG_5187.jpg", description: "High end vape"}) 
+Product.create({name: "Chocolate Kush", price: "$250", image: "https://www.lamota.org/media/images/full/cannabis-seeds/chocolate-kush.jpg", description: "High end indica!!"}) 
+Product.create({name: "Brownies", price: "$25", image: "http://i.dailymail.co.uk/i/pix/2015/03/13/269D397200000578-0-image-a-4_1426263592745.jpg", description: "High end edibles!!"}) 
+
