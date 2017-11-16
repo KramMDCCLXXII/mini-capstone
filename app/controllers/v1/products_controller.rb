f0f1e533706f0b7087ddefd13902dc3239aa1deb
Mark Custard <markc1772@gmail.com>
2017-11-15 21:20:28 -0600

    Made it restful

--- app/controllers/v1/products_controller.rb
+++ app/controllers/v1/products_controller.rb
@@ -1,6 +1,11 @@
 class V1::ProductsController < ApplicationController
-  def all_products_method
+  def index
     products = Product.all
     render json: products.as_json
   end
+#   def params_method
+#     product_id = params[input_product_id]
+#     product = Product.find_by(id: product_id)
+#     render json: product.as_json
+#   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   namespace :v1 do
-    get "/all_products_url" => "products#all_products_method"
+    get "/products" => "products#index"
+    get "/params_url" => "products#params_method"
   end 
 end
